99bafef6f81aa2dd4fef10aa57dfd663a9bc8aaf
faisal <faisaloktovian1@gmail.com>
2024-07-09 09:41:27 +0700

    fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -37,7 +46,32 @@ const Navbar = () => {
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleMenu}
       />
+
+      {isMenuOpen && (
+        <div className="absolute top-full left-0 w-full bg-white shadow-lg lg:hidden">
+          <ul className="flex flex-col items-start gap-4 p-4">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className="regular-16 text-gray-700 w-full text-left cursor-pointer py-2 transition-all hover:font-bold"
+                onClick={toggleMenu}
+              >
+                {link.label}
+              </Link>
+            ))}
+            <Button
+              type="button"
+              title="Login"
+              icon="/user.svg"
+              variant="btn_dark_green"
+              className="w-full"
+            />
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
